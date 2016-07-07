da3d3c9bf838aa305e39cbe33b2e604fcf125842
Juang JiaYu <darkevan@gmail.com>
2016-07-07 18:23:40 +0800

    Create 19_removeNthFromEnd.py

---
+++ python/19_removeNthFromEnd.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        if n == 0 or head is None:
+            return head
+        
+        dummy = ListNode(-1)
+        dummy.next = head
+        
+        fast = slow = dummy
+        
+        for i in xrange(n):
+            fast = fast.next
+            
+        while fast.next:
+            fast, slow = fast.next, slow.next
+        
+        if slow.next:
+            slow.next = slow.next.next
+        
+        return dummy.next
