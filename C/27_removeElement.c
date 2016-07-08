100fd19fb295b91f9325204aff5360f2f0afadb8
Juang JiaYu <darkevan@gmail.com>
2016-07-08 15:05:34 +0800

    Create 27_removeElement.c

---
+++ C/27_removeElement.c
@@ -0,0 +1,12 @@
+int removeElement(int* nums, int numsSize, int val) {
+    int i;
+    int len = 0;
+    for (i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[len] = nums[i];
+            len++;
+        }
+    }
+    
+    return len;
+}
